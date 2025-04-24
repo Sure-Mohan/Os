f9b0b875c7cd8fd7f489b2487ff7b1f48bfa2a0c
Sure-Mohan <151706255+Sure-Mohan@users.noreply.github.com>
2025-04-24 10:33:07 +0530

    Fifo1.c

---
+++ Fifo1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    int n, a[50], b[10], m, i, k = 0, j, c = 0, hit;
+    printf("Enter number of pages: ");
+    scanf("%d", &n);
+    printf("Enter page numbers:\n");
+    for (i = 0; i < n; i++) scanf("%d", &a[i]);
+    printf("Enter number of frames: ");
+    scanf("%d", &m);
+    for (i = 0; i < m; i++) b[i] = -1;
+
+    printf("\nRef\tString\tPage\tFrames\n");
+    for (i = 0; i < n; i++) {
+        printf("%d\t\t\t", a[i]);
+        hit = 0;
+        for (j = 0; j < m; j++)
+            if (b[j] == a[i]) hit = 1;
+
+        if (!hit) {
+            b[k] = a[i];
+            k = (k + 1) % m;
+            c++;
+        }
+        for (j = 0; j < m; j++) printf("%d\t", b[j]);
+        printf("\n");
+    }
+    printf("\nPage fault count is: %d\n", c);
+    return 0;
+}
